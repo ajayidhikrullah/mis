9eb2e7bbe506038c7d16d0adc6aa10dc7645d127
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-04-30 12:37:19 +0100

    added TutorCourses to submit to course, and tutors table

--- app/Http/Controllers/Admin/TutorController.php
+++ app/Http/Controllers/Admin/TutorController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Tutor;
 use App\Models\Course;
+use App\Models\User;
+use App\Models\Tutorcourse;
 
 class TutorController extends Controller
 {
@@ -22,10 +24,30 @@ class TutorController extends Controller
     }
 
     public function store(Request $request){
-        // $tutors = new Tutor;
-        // $tutors->full_name = $request->tutor_full_name;
-        // // $tutors->
 
+        $user = new User;
+        $course = new Course;
+        $tutors = new Tutor;
+
+        //save to users
+        $user->full_name = $request->tutor_full_name;
+        $user->email = $request->tutor_email;
+        $user->phone = $request->tutor_phone;
+        $user->address = $request->tutor_address;
+        $user->password = $request->tutor_password;
+        $user->save();
+        
+        // save to tutor relationship
+        $tutors->user_id = $user->id;
+        $tutors->save();        
+
+        $selectedTutorCourses = $request->course_id;
+        foreach ( $selectedTutorCourses as $selectedCourse) {
+            $tutorCourse = new TutorCourse;
+            $tutorCourse->tutor_id = $tutors->id;
+            $tutorCourse->course_id = $selectedCourse;            
+            $tutorCourse->save();
+        }
         
         return redirect()->route('admin.addtutors');
     }
--- resources/views/admin/tutors/add.blade.php
+++ resources/views/admin/tutors/add.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-6">    
 
-        <form>
+        <form method="POST" action="{{route('tutors.store')}}">
             <div class="form-group">
                 <label for="full_name">Tutor name</label>
                 <input type="text" name="tutor_full_name" class="form-control" id="full_name" placeholder="type in tutors full name">
@@ -18,33 +18,28 @@
 
             <div class="form-group">
                <label for="phone">Phone</label>
-                <input type="text" class="form-control" name="phone" id="phone" aria-describedby="phone" placeholder="Enter tutors Phone">
+                <input type="text" class="form-control" name="tutor_phone" id="phone" aria-describedby="phone" placeholder="Enter tutors Phone">
             </div>
 
             <div class="form-group">
                 <label for="Address">Address</label>
-                <input type="text" class="form-control" name="address" id="address" aria-describedby="address" placeholder="Enter tutor address">
+                <input type="text" class="form-control" name="tutor_address" id="address" aria-describedby="address" placeholder="Enter tutor address">
             </div>
 
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" class="form-control" name="password" id="password" aria-describedby="password" placeholder="Enter your default password">
+                <input type="password" class="form-control" name="tutor_password" id="password" aria-describedby="password" placeholder="Enter your default password">
             </div>
           
             <div class="form-group">
                 <label for="exampleSelect2">Courses</label>
-                                    
-                    
-                    <select multiple="" class="form-control" id="exampleSelect2">
+                    <select multiple="multiple" name="course_id[]" class="form-control" id="multiple exampleSelect2">
                         @foreach ($courses as $course)
-                            <option>{{$course->title}}</option>
+                            <option value="{{$course->id}}">{{$course->title}}</option>
                         @endforeach
-                        {{-- <option>5</option> --}}
                     </select>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>    
-{{-- </div> --}}
-</fieldset>
 @endsection
